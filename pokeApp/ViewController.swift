6c569b4079c61c9ec5e8f83514885c723d37210f
Oscar Navidad <oskrnav@gmail.com>
2019-11-12 16:26:41 -0600

    Initial Commit

---
+++ pokeApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pokeApp
+//
+//  Created by Oscar Navidad on 11/12/19.
+//  Copyright © 2019 Oscar Navidad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
