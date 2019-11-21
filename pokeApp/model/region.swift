a2d6c4157f21e61dc9c5f18a60b73677ec05283a
Oscar Navidad <oskrnav@gmail.com>
2019-11-21 17:00:43 -0600

    database

--- pokeApp/model/region.swift
+++ pokeApp/model/region.swift
@@ -10,8 +10,30 @@ import Foundation
 
 struct Region {
     var name:String = ""
+    var id : Int = 0
+    var pokedexes:[Pokedex] = []
+    
     
     init(data:JSON){
+        self.name = data["name"].string ?? ""
+        self.id = data["id"].int ?? 0
+        if let items = data["pokedexes"].array{
+            self.pokedexes = []
+            for item in items {
+                self.pokedexes.append(Pokedex(item))
+            }
+        }
+        
+    }
+}
+
+
+struct Pokedex{
+    var url:String = ""
+    var name:String = ""
+    
+    init(_ data:JSON){
+        self.url = data["url"].string ?? ""
         self.name = data["name"].string ?? ""
     }
 }
